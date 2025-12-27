9755c09c0723c10dcc3de06d5f80e78ab3a5c42f
Matthew Stockinger <matt@matthewstockinger.com>
2025-12-27 08:56:33 -0600

    comlete vid 19

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,25 @@
+<script>
+	import bookCover from '$assets/book_cover.png';
+	import phoneCover from '$assets/phone_cover.png';
+</script>
+
+<section class="hero">
+	<div class="hero-showcase text-center bold">
+		<h1 class="purple">MOVE TO</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="italic semi-bold">There's no need to make the same mistakes I made.</h4>
+		<img src={bookCover} alt="" class="book-cover" />
+		<img src={phoneCover} alt="" class="phone-cover" />
+	</div>
+
+	<div class="hero-text white text-center">
+		<h1>THIS EBOOK WILL</h1>
+		<h1>SAVE YOU MONEY,</h1>
+		<h1 class="mb-m">TIME &amp; NERVES</h1>
+		<p class="light-grey">
+			I moved to Spain seven yeas ago. The process was painful and difficult. I learned most things
+			the hard way: finding a job, getting a visa, calculating my cost of living, choosing an
+			apartment. That's why I wrote this e-book, to tell you how I did it. No bullshit, no stress.
+		</p>
+	</div>
+</section>
