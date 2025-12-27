bec44f5bdb9bc549ea15d6cf240c5ea506a1ff03
Matthew Stockinger <matt@matthewstockinger.com>
2025-12-27 10:54:21 -0600

    complete 23

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as HeroSection } from './HeroSection.svelte';
 export { default as Button } from './Button.svelte';
+export { default as ChapterPreview } from './ChapterPreview.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { HeroSection } from '$components';
+  import { ChapterPreview } from '$components';
 </script>
 
 <HeroSection />
+<ChapterPreview />
