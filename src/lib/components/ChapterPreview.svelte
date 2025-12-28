bcaf6c601c8852613aa75a4b3b3d75860de620c9
Matthew Stockinger <matt@matthewstockinger.com>
2025-12-28 10:10:26 -0600

    complete 27

--- src/lib/components/ChapterPreview.svelte
+++ src/lib/components/ChapterPreview.svelte
@@ -35,6 +35,9 @@
 	];
 
 	let selectedChapterNumber = $state(1);
+	let selectedChapter = $derived(
+		chapters.find((chapter) => chapter.number === selectedChapterNumber)
+	);
 
 	function selectChapter(chapter) {
 		selectedChapterNumber = chapter.number;
@@ -62,22 +65,10 @@
 		</ul>
 		<div class="chapter-info">
 			<h3 class="chapter-strapline italic mb-s">
-				Understanding Visas, Residency, and Legal Essentials
+				{selectedChapter.title}
 			</h3>
 			<p>
-				Moving to Spain involves navigating a labyrinth of bureaucratic processes that can feel
-				overwhelming at first. This chapter breaks down the essential steps required to secure your
-				visa, establish residency, and handle other legal formalities. Whether you’re planning a
-				short-term stay or seeking permanent residency, understanding Spain’s legal framework is
-				crucial. We’ll explore the different types of visas available, from student to work visas,
-				and provide tips on how to streamline your application process. Additionally, this chapter
-				covers essential documentation, such as obtaining a Número de Identidad de Extranjero (NIE),
-				which is your key to unlocking many services in Spain. We’ll also delve into the process of
-				registering with the local town hall, known as empadronamiento, and the importance of
-				understanding the tax implications of your move. Whether you’re a retiree, a digital nomad,
-				or someone relocating for work, this chapter will guide you through the maze of paperwork
-				and official procedures with practical advice and insider tips, ensuring that you’re legally
-				prepared for your new life in Spain.
+				{selectedChapter.excerpt}
 			</p>
 		</div>
 	</div>
